fdf70ba62fa680adf24deea2455f6f3d8aab8cdf
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-06-07 09:34:19 +0900

    hate

--- src/main/java/driver/DriverRun.java
+++ src/main/java/driver/DriverRun.java
@@ -4,27 +4,20 @@ import fast.Log;
 import fast.Tag;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.Duration;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class DriverRun {
-    private static final String Path = "./rsc/pic/";
-    private static final int maxPage = 3;
-    public static int maxImage = 5;
-    private static int imageCounter = 0;
-    private static boolean blockNSFW  = false;
     private static boolean isSuccess = true;
     public static String tag = "999_(hansode)";
     public static String url = "https://w1.linguaporta.jp/user/seibido/index.php";
     private static int totalImage = 0;
+    private static WebElement we = null;
 
 
     public static void main(String[] args) {
@@ -36,21 +29,21 @@ public class DriverRun {
         WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver","./opt/chromedriver_win32/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.get("https://w1.linguaporta.jp/user/seibido/index.php");
         //ウィンドウのサイズを最小化する
 //        driver.manage().window().setSize(new Dimension(0, 0));
+
+        Log.write("Nc ->");
 
         WebElement textBox = driver.findElement(By.name("id"));
         textBox.sendKeys("4534fukuda");
 
         WebElement passwordBox = driver.findElement(By.name("password"));
         passwordBox.sendKeys("vFe8drF9");
-        wait(driver,700);
+
         WebElement loginButton = driver.findElement(By.id("btn-login"));
         loginButton.click();
-
-
 
         WebElement studyForm = driver.findElement(By.name("Study"));
         studyForm.click();
@@ -61,8 +54,16 @@ public class DriverRun {
         WebElement list = driver.findElement(By.id("cn1106cn20cn30"));
         list.click();
 
-        WebElement study = driver.findElement(By.className("btn-study"));
-        study.click();
+        var we = driver.findElements(By.className("col-unitname"));
+
+        var webElement = driver.findElements(By.className("btn-study"));
+        System.out.println(webElement.size());
+        webElement.get(0).click();
+
+
+//        WebElement study = driver.findElement(By.className("btn-study"));
+//        study.click();
+//        System.out.println(study.getTagName());
 
         driver.get(url);
 
@@ -77,17 +78,21 @@ public class DriverRun {
         for(int i = 0 ; i < 10 ; i++){
             page++;
         }
+
         WebElement txtBox = driver.findElement(By.id("tabindex1"));
-        WebElement ansButton = driver.findElement(By.className("btn-answer-submit"));
+        WebElement ansButton = driver.findElement(By.id("ans_submit"));
+//        WebElement ansButton = driver.findElement(By.id("btn-logout"));
         wait(driver,10);
         WebElement que = driver.findElement(By.id("qu02"));
         System.out.println("Question : " + que.getText());
-        txtBox.sendKeys("text");
-        page ++;
+        WebElement a = driver.findElement(By.tagName("a"));
+        Log.write(a.getText() + que.getText());
+        txtBox.sendKeys("t");
+//        page ++;
         wait(driver,1200);
-        ansButton.click();
+//        ansButton.click();
 
-        wait(driver,1200);
+        wait(driver,2200);
         System.out.println(driver.getTitle() + " count : " + page);
 
 
@@ -107,9 +112,6 @@ public class DriverRun {
         return isSuccess;
     }
 
-    public static void blockExplicit(boolean blockNSFW) {
-        DriverRun.blockNSFW = blockNSFW;
-    }
 
     public static void search(String tag){
         DriverRun.tag = tag;
